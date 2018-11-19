9222ba16f342f28c3ee47579f2cb2b6f3355ae43
瞎蹦跶 <609477964@qq.com>
2018-11-19 09:48:05 +0800

    timeline
    改用timline

---
+++ Unity/Assets/Timeline/Runtime/Animation/SuperAniamtionClip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace Timeline
+{
+    [Serializable]
+    public class SuperAniamtionClip : AnimationPlayableAsset
+    {
+        
+        TimelineAsset
+    }
+
+}
